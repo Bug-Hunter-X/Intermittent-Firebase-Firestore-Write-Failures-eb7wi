f78145ad661a65805513250b46020f068c298e6d
BugHunter-X <askzenai@gmail.com>
2024-12-19 13:54:04 +0100

    Add firestoreBug.js file

---
+++ firestoreBug.js
@@ -0,0 +1,1 @@
+In a Firebase project, I encountered an issue where data wasn't being written to the Firestore database despite seemingly correct code. The write operation didn't throw any errors, yet the data remained absent from the database.  This was particularly frustrating because it happened intermittently.
